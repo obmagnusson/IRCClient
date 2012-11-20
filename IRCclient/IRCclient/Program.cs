71c4e6a9172f669ae9347f671f6d91a25f317571
obmagnusson <obmagnusson@gmail.com>
2012-11-20 15:09:23 +0000

    slkdfls

--- IRCclient/IRCclient/Program.cs
+++ IRCclient/IRCclient/Program.cs
@@ -1,37 +1,54 @@
 ﻿using System;
 using System.IO;
 using System.Collections.Generic;
-using System.Linq;
 using System.Text;
 using System.Net;
 using System.Net.Sockets;
 
 namespace IRCclient
 {
+
+	/*
+	 *  IRC Client ----------------------------------------------------------------------------
+	 *  Usage instructions: to connect to a server the usage is following from the command line:
+	 *
+	 *  c:\> IRCclient.exe "the name of the server" "Your nickname"
+	 * 
+	 *	This client receives one line from the server and waits for input from the user and then 
+	 *	then receives the next line from the server and so on. To terminate the session the QUIT 
+	 *	command is issued. 
+	 * 
+	 *	Written by : 
+	 *  Edward Alexander Eiríksson 
+	 *  Ólafur Björn Magnússon 
+	 *  Sindri Már Sigfússon
+	 *  --------------------------------------------------------------------------------------
+	 */	  
 	class Program
 	{
-		public string getDate() 
+		public string GetDate() 
 		{
 			DateTime time = DateTime.Now;
 			string format = "ddd M yyyy hh:mm:ss ";
 			return time.ToString(format);
 		}
 
-		public void ircInit(StreamWriter serverWrite, string Nick)
+		public void IrcInit(StreamWriter serverWrite, string nick)
 		{
 			serverWrite.WriteLine("CAP LS");
 			serverWrite.Flush();
-			serverWrite.WriteLine("NICK " + Nick);
+			serverWrite.WriteLine("NICK " + nick);
 			serverWrite.Flush();
-			serverWrite.WriteLine("USER " + Nick + " 0 * :...");
+			serverWrite.WriteLine("USER " + nick + " 0 * :...");
 			serverWrite.Flush();
 			serverWrite.WriteLine("CAP REQ :multi-prefix");
 			serverWrite.Flush();
 			serverWrite.WriteLine("CAP END");
 			serverWrite.Flush();
-			serverWrite.WriteLine("USERHOST " + Nick);
+			serverWrite.WriteLine("USERHOST " + nick);
 			serverWrite.Flush();
 		}
+
         public StreamWriter OpenLogFile()
         {
             StreamWriter log;
@@ -49,32 +66,29 @@ namespace IRCclient
 		static void Main(string[] cmdLine)
 		{
 			Program irc = new Program();
-
 			int port = 6667;
 			String hostName = (String)cmdLine.GetValue(0);
-            String Nick = (String)cmdLine.GetValue(1);
-			
+            String nick = (String)cmdLine.GetValue(1);		
 			TcpClient client = null;
+
 			try
 			{
 				client = new TcpClient(hostName, port);
-				Stream s = client.GetStream();
+				Stream serverStream = client.GetStream();
                 StreamWriter log = irc.OpenLogFile();
-				StreamReader serverRead = new StreamReader(s);
-				StreamWriter serverWrite = new StreamWriter(s);
+				StreamReader serverRead = new StreamReader(serverStream);
+				StreamWriter serverWrite = new StreamWriter(serverStream);
 				Console.WriteLine(serverRead.ReadLine());
 
-
-
 				//Start IrcServer Session
-				irc.ircInit(serverWrite, Nick);
+				irc.IrcInit(serverWrite, nick);
 				while (true)
 				{
 					Console.Write(">: ");
 					string userInput = Console.ReadLine();
 					Console.WriteLine(userInput);
 
-					log.WriteLine(irc.getDate() + "GMT : Client: " + userInput);
+					log.WriteLine(irc.GetDate() + "GMT : Client: " + userInput);
 					serverWrite.Flush();
 
 					if (userInput == "QUIT")
@@ -82,22 +96,22 @@ namespace IRCclient
 						client.Close();
 						break;
 					}
-
 					Console.WriteLine("IRC SERVER : " + serverRead.ReadLine());
-					log.WriteLine(irc.getDate() + "GMT : Server: " + serverRead.ReadLine());
+					log.WriteLine(irc.GetDate() + "GMT : Server: " + serverRead.ReadLine());
 				}
-				s.Close();
+				serverStream.Close();
 				log.Close();
 			}
 			catch (System.Net.Sockets.SocketException ex)
 			{
-				Console.Error.WriteLine("Could find host..");
+				Console.Error.WriteLine("Error : IRC connection failed!");
+				Console.Error.WriteLine("Please contact technical support(Freysteinn).");
 			}
 			finally
 			{
+				if(client != null)
 				client.Close();
 			}
-
 			Console.WriteLine("Press any key to continue ....");
 			Console.ReadKey(true);
 		}
